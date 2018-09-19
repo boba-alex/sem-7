9b67383ebd171c264754fd2464c9413223ffe753
boba-alex <099@tut.by>
2018-09-19 17:52:52 +0300

    edited

--- ISM/Labs/src/main/java/Lab2/ModelingDiscreteRandomVariable.java
+++ ISM/Labs/src/main/java/Lab2/ModelingDiscreteRandomVariable.java
@@ -27,42 +27,48 @@ public class ModelingDiscreteRandomVariable {
 
 	private static final int N = 1000;
 	private static final double E = 0.05;
-	private static final double p1 = 0.2;
 	private static final double p2 = 0.6;
 
 	public static void main(String[] args) {
+		negativeBinomial();
 
-		int[] list1 = new int[N];
+
+
+	}
+
+	private static void bernulli(){
+		double p = 0.2;
+		int[] list = new int[N];
 		int[] v = new int[2]; //частота 0 и 1
-		double average1 = 0;
-		double disp1 = 0;
+		double average = 0;
+		double disp = 0;
 
 		System.out.println("Бернулли");
 		Random random = new Random();
 		for (int i = 0; i < N; i++) {
 			double a = random.nextDouble();
 
-			if (a <= p1) {
-				list1[i] = 1;
+			if (a <= p) {
+				list[i] = 1;
 				v[1]++;
 			} else {
-				list1[i] = 0;
+				list[i] = 0;
 				v[0]++;
 			}
 
-			average1 += list1[i];
+			average += list[i];
 		}
 		// несмещенная оценка мат. ожидания
 		//https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D1%81%D0%BC%D0%B5%D1%89%D1%91%D0%BD%D0%BD%D0%B0%D1%8F_%D0%BE%D1%86%D0%B5%D0%BD%D0%BA%D0%B0
-		average1 /= 1000.0;
+		average /= 1000.0;
 		// https://ru.wikipedia.org/wiki/%D0%92%D1%8B%D0%B1%D0%BE%D1%80%D0%BE%D1%87%D0%BD%D0%B0%D1%8F_%D0%B4%D0%B8%D1%81%D0%BF%D0%B5%D1%80%D1%81%D0%B8%D1%8F
 		// несмещённая (исправленная) дисперсия
 		for (int i = 0; i < N; i++) {
-			disp1 += Math.pow(list1[i] - average1, 2);
+			disp += Math.pow(list[i] - average, 2);
 		}
-		disp1 /= 999.0;
+		disp /= 999.0;
 
-		double x1 = Math.pow(v[1] - N * p1, 2) / (N * p1) + Math.pow(v[0] - N * (1 - p1), 2) / (N * (1 - p1));
+		double x1 = Math.pow(v[1] - N * p, 2) / (N * p) + Math.pow(v[0] - N * (1 - p), 2) / (N * (1 - p));
 
 		int numberOfObservatedGradations = 2; // 0 or 1
 		int numberOfDegreesOfFreedom = numberOfObservatedGradations - 1;
@@ -73,8 +79,99 @@ public class ModelingDiscreteRandomVariable {
 		if (x1 < tableX2) {
 			System.out.println("x1 = " + x1 + " < tableX2");
 		}
-		System.out.println("Average = " + average1 + ", but true value = " + p1);
-		System.out.println("Dispersia = " + disp1 + ", but true value = " + p1 * (1 - p1));
+		System.out.println("Average = " + average + ", but true value = " + p);
+		System.out.println("Dispersia = " + disp + ", but true value = " + p * (1 - p));
 	}
 
+//	private static void geometric(){
+//		double p = 0.6;
+//		double q = 1 - p;
+//		int[] list = new int[N];
+//		int[] v = new int[Integer.MAX_VALUE]; //частота 0 и 1
+//		double average = 0;
+//		double disp = 0;
+//
+//		System.out.println("Геометрическое распределение");
+//		Random random = new Random();
+//		for (int i = 0; i < N; i++) {
+//			double a = random.nextDouble();
+//			int x = (int)Math.round(Math.log(a) / Math.log(q));
+//			list[i] = x;
+//				v[x]++;
+//			}
+//
+//			average += list[i];
+//		}
+//		// несмещенная оценка мат. ожидания
+//		//https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D1%81%D0%BC%D0%B5%D1%89%D1%91%D0%BD%D0%BD%D0%B0%D1%8F_%D0%BE%D1%86%D0%B5%D0%BD%D0%BA%D0%B0
+//		average /= 1000.0;
+//		// https://ru.wikipedia.org/wiki/%D0%92%D1%8B%D0%B1%D0%BE%D1%80%D0%BE%D1%87%D0%BD%D0%B0%D1%8F_%D0%B4%D0%B8%D1%81%D0%BF%D0%B5%D1%80%D1%81%D0%B8%D1%8F
+//		// несмещённая (исправленная) дисперсия
+//		for (int i = 0; i < N; i++) {
+//			disp += Math.pow(list[i] - average, 2);
+//		}
+//		disp /= 999.0;
+//
+//		double x1 = Math.pow(v[1] - N * p, 2) / (N * p) + Math.pow(v[0] - N * (1 - p), 2) / (N * (1 - p));
+//
+//		int numberOfObservatedGradations = 2; // 0 or 1
+//		int numberOfDegreesOfFreedom = numberOfObservatedGradations - 1;
+//		// table value
+//		ChiSquaredDistribution chiSquaredDistribution = new ChiSquaredDistribution(numberOfDegreesOfFreedom, ChiSquaredDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY);
+//		double tableX2 = chiSquaredDistribution.inverseCumulativeProbability(1 - E);
+//		System.out.println("Table χ2 = " + tableX2);
+//		if (x1 < tableX2) {
+//			System.out.println("x1 = " + x1 + " < tableX2");
+//		}
+//		System.out.println("Average = " + average + ", but true value = " + p);
+//		System.out.println("Dispersia = " + disp + ", but true value = " + p * (1 - p));
+//	}
+
+	private static void negativeBinomial(){
+		double r = 5;
+		double p = 0.25;
+		int[] list = new int[N];
+		int[] v = new int[2]; //частота 0 и 1
+		double average = 0;
+		double disp = 0;
+
+		System.out.println("Бернулли");
+		Random random = new Random();
+		for (int i = 0; i < N; i++) {
+			double a = random.nextDouble();
+
+			if (a <= p) {
+				list[i] = 1;
+				v[1]++;
+			} else {
+				list[i] = 0;
+				v[0]++;
+			}
+
+			average += list[i];
+		}
+		// несмещенная оценка мат. ожидания
+		//https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D1%81%D0%BC%D0%B5%D1%89%D1%91%D0%BD%D0%BD%D0%B0%D1%8F_%D0%BE%D1%86%D0%B5%D0%BD%D0%BA%D0%B0
+		average /= 1000.0;
+		// https://ru.wikipedia.org/wiki/%D0%92%D1%8B%D0%B1%D0%BE%D1%80%D0%BE%D1%87%D0%BD%D0%B0%D1%8F_%D0%B4%D0%B8%D1%81%D0%BF%D0%B5%D1%80%D1%81%D0%B8%D1%8F
+		// несмещённая (исправленная) дисперсия
+		for (int i = 0; i < N; i++) {
+			disp += Math.pow(list[i] - average, 2);
+		}
+		disp /= 999.0;
+
+		double x1 = Math.pow(v[1] - N * p, 2) / (N * p) + Math.pow(v[0] - N * (1 - p), 2) / (N * (1 - p));
+
+		int numberOfObservatedGradations = 2; // 0 or 1
+		int numberOfDegreesOfFreedom = numberOfObservatedGradations - 1;
+		// table value
+		ChiSquaredDistribution chiSquaredDistribution = new ChiSquaredDistribution(numberOfDegreesOfFreedom, ChiSquaredDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY);
+		double tableX2 = chiSquaredDistribution.inverseCumulativeProbability(1 - E);
+		System.out.println("Table χ2 = " + tableX2);
+		if (x1 < tableX2) {
+			System.out.println("x1 = " + x1 + " < tableX2");
+		}
+		System.out.println("Average = " + average + ", but true value = " + p);
+		System.out.println("Dispersia = " + disp + ", but true value = " + p * (1 - p));
+	}
 }
