64b5f933baaa61e01be10e56993f68719e6e74aa
bobaalex <099@tut.by>
2018-11-11 23:21:45 +0200

    Lab 4 ism integrate2 added

--- ISM/Labs/src/main/java/Lab4/MonteCarlo.java
+++ ISM/Labs/src/main/java/Lab4/MonteCarlo.java
@@ -12,6 +12,7 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class MonteCarlo {
@@ -25,6 +26,7 @@ public class MonteCarlo {
 	public static void main(String[] args) {
 
 		testExample1();
+		testExample2();
 	}
 
 	public static void testExample1() {
@@ -47,16 +49,64 @@ public class MonteCarlo {
 	public static double integrate(double a, double b, int n, Function<Double, Double> g) {
 
 		double integralResult = 0;
+		//плотность вероятности равномерного распределения
 		double p = p(a, b);
 		Random random = new Random();
 		for (int i = 0; i < n; i++) {
 
-			double y = random.nextDouble();
-			double x = (b - a) * y + a;
+			// генерирование равномерно распределенной случайной величины
+			double r = random.nextDouble();
+			double x = (b - a) * r + a;
 			integralResult += g.apply(x) / p;
 		}
 		integralResult /= n;
 		return integralResult;
+	}
+
+	public static void testExample2() {
+
+		int n = 10;
+		//1 <= x^2 + y^2 <3
+		//то есть интегрирую по области
+		double a = -Math.sqrt(3);
+		double b = Math.sqrt(3);
+		//calculate properly!
+		Double trueIntegralValue = -0.485736;
+		BiFunction<Double, Double, Double> g = (x, y) -> 1 / (Math.pow(x, 2) + Math.pow(y, 4));
+
+		List<Pair<Integer, Double>> results = new ArrayList<>();
+		for (int i = n; i < n * 100; i += n) {
+			double integralResult = integrate2(a, b, n, g);
+			//double difference = Math.abs(integralResult - trueIntegralValue);
+			results.add(new Pair<>(i, integralResult));
+		}
+		show(results, trueIntegralValue);
+	}
+
+	public static double integrate2(double a, double b, int n, BiFunction<Double, Double, Double> g) {
+
+		double integralResult = 0;
+		//плотность вероятности равномерного распределения
+		double p = p(a, b);
+		Random random = new Random();
+		for (int i = 0; i < n; i++) {
+
+			// генерирование равномерно распределенной случайно величины
+			double r1 = random.nextDouble();
+			double x = 1;//(b - a) * r1 + a;
+			double r2 = random.nextDouble();
+			double y = 1;//(b - a) * r2 + a;
+			if (between(x * x + y * y, 1, 3)) {
+				integralResult += g.apply(x, y) / (p * p);
+			}
+		}
+		integralResult /= n;
+		return integralResult;
+	}
+
+	public static boolean between(double x, double a, double b) {
+
+		return x >= a && x <= b;
 	}
 
 	public static void show(List<Pair<Integer, Double>> points, Double trueIntegralValue) {
