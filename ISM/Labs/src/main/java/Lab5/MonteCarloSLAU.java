4be954a9a42d946f8fd1686acb76f3409ce987e5
bobaalex <099@tut.by>
2018-11-13 20:34:18 +0200

    Fix example from book

--- ISM/Labs/src/main/java/Lab5/MonteCarloSLAU.java
+++ ISM/Labs/src/main/java/Lab5/MonteCarloSLAU.java
@@ -4,30 +4,33 @@ import java.util.Random;
 
 public class MonteCarloSLAU {
 
+	static int n = 2; //размерность системы
+	static double x = 0, y = 0; // решение системы
+
+	//x = Ax + f
+	//все собств зн-я А меньше единицы
+	static double[][] a;//{{-0.1, 0.8}, {0.4, -0.1}};
+	static double[] f;//{0.1, -0.2};
+
+	static double[] h = {0, 1};
+	static double[] hx = {1, 0}; // h для вычисления x
+	static double[] hy = {0, 1}; // h для вычисления y
+	static double[] pi; // вектор нач состояний цепи маркова
+	static double[][] p; //матрица переходных вероятностей
+	static int N = 1000; // длина Цепи Маркова
+	static int[] i = new int[N + 1]; // цепь Маркова
+	static double[] Q = new double[N + 1]; //веса состояний цепи Маркова
+	static int m = 10000; // кол-во реализаций цепи Маркова
+	static double[] ksi = new double[m]; //СВ
+	static double alpha; // БСВ
+
 	public static void main(String[] args) {
 
-		int n = 2; //размерность системы
-		double x = 0, y = 0; // решение системы
-		double[][] a1 = {{1.1, -0.8}, {-0.7, -0.3}};
+		test1();
+	}
 
-		double[] f1 = {0.1, 0.1};
+	public static double calculate(double[] h) {
 
-		//x = Ax + f
-		//все собств зн-я А меньше единицы
-		double[][] a = {{-0.1, 0.8}, {0.4, -0.1}};
-		double[] f = {0.1, -0.2};
-
-		double[] h = {0, 1};
-		double[] hx = {1, 0}; // h для вычисления x
-		double[] hy = {0, 1}; // h для вычисления y
-		double[] pi = {0.5, 0.5}; // вектор нач состояний цепи маркова
-		double[][] P = {{0.5, 0.5}, {0.5, 0.5}}; //матрица переходных вероятностей
-		int N = 1000; // длина Цепи Маркова
-		int[] i = new int[N + 1]; // цепь Маркова
-		double[] Q = new double[N + 1]; //веса состояний цепи Маркова
-		int m = 10000; // кол-во реализаций цепи Маркова
-		double[] ksi = new double[m]; //СВ
-		double alpha; // БСВ
 		Random random = new Random();
 
 		//Моделирую m цепей Маркова длины N
@@ -58,13 +61,14 @@ public class MonteCarloSLAU {
 
 			for (int k = 1; k <= N; k++) {
 
-				if (P[i[k - 1]][i[k]] > 0) {
-					Q[k] = Q[k - 1] * a[i[k - 1]][i[k]] / P[i[k - 1]][i[k]];
+				if (p[i[k - 1]][i[k]] > 0) {
+					Q[k] = Q[k - 1] * a[i[k - 1]][i[k]] / p[i[k - 1]][i[k]];
 				} else {
 					Q[k] = 0;
 				}
 			}
 
+			ksi[j] = 0;// set to 0 before calculation
 			for (int k = 0; k <= N; k++) {
 
 				ksi[j] = ksi[j] + Q[k] * f[i[k]]; // строю Кси-N по j-ой цепи Маркова
@@ -72,14 +76,32 @@ public class MonteCarloSLAU {
 		}
 
 		// среднее значение
-
+		double x = 0;
 		for (int k = 0; k < m; k++) {
 
-			x = x + ksi[k];
+			x += ksi[k];
 		}
 		x /= m;
 
+		return x;
+	}
+
+	public static void test1() {
+
+		//double[][] a1 = {{1.1, -0.8}, {-0.7, -0.3}};
+		//double[] f1 = {0.1, 0.1};
+		//x = Ax + f
+		//все собств зн-я А меньше единицы
+		a = new double[][] {{-0.1, 0.8}, {0.4, -0.1}};
+		f = new double[] {0.1, -0.2};
+
+		pi = new double[] {0.5, 0.5};
+		p = new double[][] {{0.5, 0.5}, {0.5, 0.5}};
+
+		x = calculate(hx);
+		y = calculate(hy);
 		System.out.println(x);
+		System.out.println(y);
 	}
 
 }
