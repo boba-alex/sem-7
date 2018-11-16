2a00372711fc869754b68fd27c011a8805176e47
boba-alex <099@tut.by>
2018-11-16 21:30:36 +0300

    iis lab 1 fix

--- IIS/Labs/src/main/Lab1/Form.java
+++ IIS/Labs/src/main/Lab1/Form.java
@@ -1,40 +1,50 @@
 package main.Lab1;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.awt.*;
 import java.util.List;
 
 public class Form extends JFrame {
+
 	public static boolean WAIT = true;
-	Main main = new Main();
 
 	private JComboBox comboBox1;
-	private JButton button1;
 
 	public static void main(String[] args) {
-		Form form = new Form();
-		form.setVisible(true);
-		form.initialize();
+
+		//Form form = new Form();
+		//form.setVisible(true);
+//		new Runnable() {
+//
+//			@Override
+//			public void run() {
+//
+//				Form form = new Form();
+//				form.setSize(new Dimension(200, 200));
+//				form.setMinimumSize(new Dimension(200, 200));
+//				form.setVisible(true);
+//				form.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//			}
+//		};
+		new Runnable() {
+
+			@Override
+			public void run() {
+
+				new Main().analyzeBDOfRules(new Statement(Attribute.HAS_NAME, null));
+			}
+		};
+		//form.initialize();
 	}
 
-	public String chooseValue(List<String> values){
-		for(String v : values){
+	public String chooseValue(List<String> values) {
+
+		for (String v : values) {
 			comboBox1.addItem(v);
 		}
 		WAIT = true;
-		while(WAIT);
+		while (WAIT)
+			;
 		return comboBox1.getSelectedItem().toString();
 	}
-
-	public void initialize(){
-		button1.addActionListener(new AbstractAction() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				main.analyzeBDOfRules(new Statement("pups", null, null));
-				WAIT = false;
-			}
-		});
-	}
-
 }
